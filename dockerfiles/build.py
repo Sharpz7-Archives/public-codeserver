d20a0518edd8c9610ffd5b604112610b246eda40
Sharpz7 <adam@mcaq.me>
2022-06-07 16:28:58 +0000

    Bugs fixed

--- dockerfiles/build.py
+++ dockerfiles/build.py
@@ -48,27 +48,31 @@ def main():
     for loc in locs:
         if loc not in BANNED:
             builds.append(Build(loc))
+
     split = split_builds(builds, EXECUTOR_COUNT)
 
     for num, executor in enumerate(split):
         job_code = ""
 
-        for build in executor:
-            shell = (
-                "set -e\n"
-                f"docker login -u $DOCKER_USER -p $DOCKER_PASS"
-                f"docker pull sharp6292/{build.name}:{build.version} || true\n"
-                f"docker build --cache-from sharp6292/{build.name}:{build.version}"
-                f" -f dockerfiles/{build.folder}/Dockerfile "
-                f"-t sharp6292/{build.name}:{build.version} .\n"
-                f"docker login -u $DOCKER_USER -p $DOCKER_PASS"
-                f"docker push sharp6292/{build.name}:{build.version}\n"
-            )
-            job_code += shell
+        if executor != []:
+            for build in executor:
+                shell = (
+                    "set -e\n"
+                    f"docker login -u $DOCKER_USER -p $DOCKER_PASS\n"
+                    f"docker pull sharp6292/{build.name}:{build.version} || true\n"
+                    f"docker build --cache-from sharp6292/{build.name}:{build.version}"
+                    f" -f dockerfiles/{build.folder}/Dockerfile "
+                    f"-t sharp6292/{build.name}:{build.version} .\n"
+                    f"docker push sharp6292/{build.name}:{build.version}\n"
+                )
+                job_code += shell
+
+        else:
+            job_code = "echo 'No builds to build'\n"
 
         with open(f'./builds/build-{num}.sh', 'w') as file:
             file.write(job_code)
-            print(shell)
+            print(job_code)
 
 
 if __name__ == "__main__":
