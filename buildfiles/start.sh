3f01845f6123644d2e2c20fbc8874d8c17644bf7
Sharpz7 <adam@mcaq.me>
2022-06-09 16:09:26 +0000

    Added sudo

--- buildfiles/start.sh
+++ buildfiles/start.sh
@@ -3,6 +3,6 @@
 # Replace commas with spaces
 SHARPDOMAINS=$(echo $SHARPDOMAINS | sed 's/,/ /g')
 
-sed -i "s/XXXXX/$SHARPDOMAINS/g" /sharpnet/nginx.conf
+sudo sed -i "s/XXXXX/$SHARPDOMAINS/g" /sharpnet/nginx.conf
 
 code-server --auth password
